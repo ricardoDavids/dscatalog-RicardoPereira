bf42a1800a19f3dceab962b907344f63a113a0b1
Ricardo Pereira <ricardo.david2020.new@gmail.com>
2022-09-27 00:06:31 +0100

    Update category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,14 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice// Isto é que vai permitir que a classe abaixo intercepte alguma excepção que acontecer lá na camada de Controllador REST E DEPOIS ele é que vai tratar a excepçao agora  
 public class ResourceExceptionHandler {
 
 	//Este metodo infra vai ser uma resposta de requesição onde o pay loader, ou seja, o conteudo da resposta vai ser um objecto do tipo standardError
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e,HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e,HttpServletRequest request){
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
 		err.setStatus(HttpStatus.NOT_FOUND.value());
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service // Todo o framework moderno tem algum mecanismo de injecção de dependencia automatizado 
 public class CategoryService {
@@ -29,7 +31,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category>obj= repository.findById(id);// o retorno dessa busca nunca vai ser um objecto nulo, ai sim, dentro desse optional eu vou poder ter ou não a Categoria lá dentro.
-		Category entity = obj.orElseThrow(()-> new EntityNotFoundException("Entity not found "));// O metodo do get do Optional ele obtem aquele objecto que estava dentro do optional, entao obtendo essa entidade que eu busquei dentro do banco de dados, irei mandar retornar dentro do meu metodo
+		Category entity = obj.orElseThrow(()-> new ResourceNotFoundException("Entity not found "));// O metodo do get do Optional ele obtem aquele objecto que estava dentro do optional, entao obtendo essa entidade que eu busquei dentro do banco de dados, irei mandar retornar dentro do meu metodo
 		return new CategoryDTO(entity);
 	}
 	
@@ -39,5 +41,24 @@ public class CategoryService {
 		entity.setName(dto.getName());
 		entity=repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+		/*Qual a diferença do findById para o getOne(id)? O findById, ele efectiva o acesso ao bd, ele vai lá no banco de dados(bd), trás os dados daquele objecto que vc procurou. O getOne ele nao toca no bd, ele vai instanciar um objecto provisorio com os dados ali e com esse Id naquele objecto
+		 * Só quando vc mandar salvar, ai sim é que ele vai no banco de dados, essa é a diferença, entao quando vc quiser atualizar um dado, vc usa o getOne para não ter que ir ate ao banco de dados sem necessidade  */
+		//Agora que aqui já estou com a entidade Category instanciada na memoria, eu irei atualizar os dados dela.
+		
+		// Quais são os dados aqui de atualização que vem no dto? No caso da categoria é somente o nome.
+		Category entity = repository.getOne(id); //Neste caso aqui, em versoes mais recentes do Spring Boot, o nome da função mudou para: getReferenceById
+		entity.setName(dto.getName());
+		entity= repository.save(entity);
+		return new CategoryDTO(entity);
+		}
+		catch(EntityNotFoundException e) { // isto é no caso de eu tentar chamar o getOne e dar um ID que não existe e depois tentar salvar, ele vai estourar para mim essa (EntityNotFoundException e) e caso ela ocorra, ai sim, eu vou lançar a minha excepção personalizada que agora é ResourceNotFoundException
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		
 	}
 }
