023b3cb94df00f9edd819b6be26903e893ddced0
Ricardo Pereira <ricardo.david2020.new@gmail.com>
2022-09-28 17:01:46 +0100

    Pagination

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,14 +1,14 @@
 package com.devsuperior.dscatalog.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,10 +25,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
-		List<Category> list =repository.findAll(); // Fui lá no repository e busquei tds as categorias no banco de dados e guardei nessa lista de categorias, agora vou ter que converter essa Lista de Categorias para uma lista de CategoriaDTO
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
+		Page<Category> list =repository.findAll(pageRequest);//Chamamos a busca paginada lá do repository,agora vamos ter que converter a lista de Category para a colecção equivalente a CategoryDTO   // Fui lá no repository e busquei tds as categorias no banco de dados e guardei nessa lista de categorias, agora vou ter que converter essa Lista de Categorias para uma lista de CategoriaDTO
 		
-		return list.stream().map(x->new CategoryDTO(x)).collect(Collectors.toList());
+		return list.map(x->new CategoryDTO(x));
 	}
 
 	@Transactional(readOnly = true)
